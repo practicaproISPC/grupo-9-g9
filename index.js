ae9f98d147e591395e856795c55fc10599b95d40
gustavo <gutu3@hotmail.com>
2021-11-22 17:11:57 -0300

    para heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hola si puedes leer esto estas conectado la localhost !')
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)});
